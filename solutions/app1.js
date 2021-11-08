f18db06dd68dbcd055845c2cd7aa7704ba19eef6
keszigabesz <bajo.gabor@gmail.com>
2021-11-08 18:56:36 +0100

    félig kész

--- solutions/app1.js
+++ solutions/app1.js
@@ -1,7 +1,7 @@
+const removeDuplicatesFromArray = array => {
+    return array.filter((item, index) => array.indexOf(item) === index);
+}
 
 
 
-
-
-
-// export default removeDuplicatesFromArray
+export default removeDuplicatesFromArray;
